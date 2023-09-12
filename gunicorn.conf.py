2f42e4ef12d519a82ad827e18c4028202f1f7771
HansolLim <dev.hansollim@gmail.com>
2023-09-12 17:59:21 +0900

    443

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,6 @@
-bind = "0.0.0.0:80"
+bind = "0.0.0.0:443"
 workers = 1
 name = "hub"
+
+certfile = "/etc/letsencrypt/live/hub.re.kr/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/hub.re.kr/privkey.pem"
